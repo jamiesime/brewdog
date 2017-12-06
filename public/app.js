0019bd464f26ca3a6d12c2f9a2e31705fa5a56fe
Jamie Sime <jamiesime@gmail.com>
2017-12-06 17:00:36 +0000

    added xhr request functions for beer api

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
-var app = function(){
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener('load', callback);
+  request.send();
+}
 
+var requestComplete = function(){
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  var beers = JSON.parse(jsonString);
+}
+
+
+var app = function(){
+  var url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
+
+  //go ahead and do the XHR stuff first
 }
 
 window.addEventListener('load', app);
